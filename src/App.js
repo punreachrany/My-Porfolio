13a5bf3c4a299d3e6b6cdf91297c2facb9d81275
Punreach Rany <punreach.rany@yahoo.com>
2024-10-03 14:10:28 +0900

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import Contact from './sections/Contact/Contact';
+import Footer from './sections/Footer/Footer';
+import Hero from './sections/Hero/Hero';
+import Projects from './sections/Projects/Projects';
+import Skills from './sections/Skills/Skills';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Hero />
+      <Projects />
+      <Skills />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,37 @@
+import styles from './SkillsStyles.module.css';
+import checkMarkIconDark from '../../assets/checkmark-dark.svg';
+import checkMarkIconLight from '../../assets/checkmark-light.svg';
+import SkillList from '../../common/SkillList';
+import { useTheme } from '../../common/ThemeContext';
+
+function Skills() {
+  const { theme } = useTheme();
+  const checkMarkIcon = theme === 'light' ? checkMarkIconLight : checkMarkIconDark;
+
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="IOS(Swift)" />
+        <SkillList src={checkMarkIcon} skill="Android(Kotlin)" />
+        <SkillList src={checkMarkIcon} skill="Flutter" />
+        <SkillList src={checkMarkIcon} skill="React Native" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="ReactJS" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+        <SkillList src={checkMarkIcon} skill="TypeScript" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Django" />
+        <SkillList src={checkMarkIcon} skill="Node" />
+        <SkillList src={checkMarkIcon} skill="Docker" />
+        <SkillList src={checkMarkIcon} skill="Kubernetes" />
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
